3037ce755940de3a64473074ef622d7176956ff7
JennieChu713 <38804711+JennieChu713@users.noreply.github.com>
2021-04-22 18:09:31 +0800

    compeleted: hw1.js, hw2.js, hw3.js, hw4.js, hw5.js and hw6.md.

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,11 @@
 function capitalize(str) {
-  
+    'use strict';
+    let capital = str[0];
+    const re = /[a-z]/;
+    if (capital.match(re)) {
+        return String.fromCharCode(capital.charCodeAt()-32) + str.slice(1);
+    }
+    return str;
 }
 
 console.log(capitalize('hello'));
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,9 +1,24 @@
 function join(arr, concatStr) {
-  
-}
+    'use strict';
+    if (arr.length < 2) {
+        return arr.toString();
+    }
 
-function repeat(str, times) {
+    let conjoin = "";
+    for (let i = 0; i < arr.length-1; i++) {
+        conjoin += arr[i] + concatStr;
+    }
+    conjoin += arr.slice(-1);
+    return conjoin;
+}
   
+function repeat(str, times) {
+    'use strict';
+    let multiStr = "";
+    for (let i = 0; i < times; i++) {
+        multiStr += str;
+    }
+    return multiStr;
 }
 
 console.log(join(['a'], '!'));
