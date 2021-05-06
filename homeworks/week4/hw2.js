ef3e9a9f7272a966cbe74bb0d3b4c7459bcdc670
JennieChu713 <38804711+JennieChu713@users.noreply.github.com>
2021-05-06 09:42:13 +0800

    Modified: hw1.js and hw2.js.

--- homeworks/week4/hw2.js
+++ homeworks/week4/hw2.js
@@ -1,14 +1,23 @@
 const request = require('request')
 const process = require('process')
 
+const callback = (err, res, body) => {
+  const datas = JSON.parse(body)
+  if (datas.length) {
+    for (const { id, name } of datas) {
+      console.log(id, name)
+    }
+  } else {
+    const { id, name } = datas
+    console.log(id, name)
+  }
+}
+
 if (process.argv.length === 4) {
   const action = process.argv[2]
   const modify = process.argv[3]
   if (action === 'read') {
-    request(`https://lidemy-book-store.herokuapp.com/books/${modify}`, (err, res, body) => {
-      const { id, name } = JSON.parse(body)
-      console.log(id, name)
-    })
+    request(`https://lidemy-book-store.herokuapp.com/books/${modify}`, callback)
   } else if (action === 'delete') {
     request.del(`https://lidemy-book-store.herokuapp.com/books/${modify}`, (err, res, body) => {
       console.log('id:', modify, 'deleted')
@@ -16,27 +25,18 @@ if (process.argv.length === 4) {
   } else if (action === 'create') {
     request.post('https://lidemy-book-store.herokuapp.com/books',
       { form: { name: modify } },
-      (err, res, body) => {
-        const { id, name } = JSON.parse(body)
-        console.log('created: ', id, name)
-      }
+      callback
     )
   }
-} else if (process.argv[2] === 'list') {
-  request('https://lidemy-book-store.herokuapp.com/books?_limit=20', (err, res, body) => {
-    for (const { id, name } of JSON.parse(body)) {
-      console.log(id, name)
-    }
-  })
-} else if (process.argv[2] === 'update') {
+} else if (process.argv[2] === 'list' && process.argv.length === 3) {
+  // 根據題意，因為是指定列出前20本書而非全部的書，因此加上 query-string 限定在20本
+  request('https://lidemy-book-store.herokuapp.com/books?_limit=20', callback)
+} else if (process.argv[2] === 'update' && process.argv.length === 5) {
   const modify = process.argv[3]
   const newName = process.argv[4]
   /* eslint-disable-next-line */
   request.patch(`https://lidemy-book-store.herokuapp.com/books/${modify}`,
     { form: { name: newName } },
-    (err, res, body) => {
-      const { name } = JSON.parse(body)
-      console.log('updated ', name)
-    }
+    callback
   )
 }
